eef1dc5e942700128181b3bbf0bf1184e31f51fd
fabprojectmanger <91474850+fabprojectmanger@users.noreply.github.com>
2023-11-01 19:08:07 +0530

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -14,7 +14,7 @@ module.exports = {
   serverConditions: ['worker', process.env.NODE_ENV],
   serverDependenciesToBundle: 'all',
   serverModuleFormat: 'esm',
-  serverPlatform: 'neutral',
+  serverPlatform: "neutral",
   serverMinify: process.env.NODE_ENV === 'production',
   postcss: true,
   tailwind: true,
