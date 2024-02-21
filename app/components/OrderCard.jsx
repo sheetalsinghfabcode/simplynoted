d51baf80a043ba9be53258d4b62f56252c08ffd9
sheetalsinghfabcode <sheetal@thefabcode.org>
2024-02-21 15:33:39 +0530

    update in route

--- app/components/OrderCard.jsx
+++ app/components/OrderCard.jsx
@@ -3,78 +3,85 @@ import {Heading, Text, Link} from '~/components';
 import {statusMessage} from '~/lib/utils';
 import DynamicButton from './DynamicButton';
 import placeholderImage from '../../assets/Image/product-placeholder.png';
+import {useState} from 'react';
+import OrderRoute from '../routes/($locale).account.address.$id';
+import Instruction from './modal/Instruction';
 
 export function OrderCard({order}) {
   if (!order?.id) return null;
   const [legacyOrderId, key] = order.id.split('/').pop().split('?');
   const lineItems = flattenConnection(order?.lineItems);
 
+  const [showOrderDetails, setShowOrderDetails] = useState(false);
+
   return (
-    <li className="grid text-center shadow-lg">
-      <Link
-        className=" flex md:grid items-center justify-center gap-4 p-4 md:gap-6 md:p-6  md:grid-cols-2"
-        to={`/account/orders/${legacyOrderId}?${key}`}
-        prefetch="intent"
-      >
-        {lineItems[0].variant?.image ? (
-          <div className="card-image aspect-square bg-primary/5">
-            <Image
-              width={168}
-              height={168}
-              className="w-full fadeIn cover"
-              alt={lineItems[0].variant?.image?.altText ?? 'Order image'}
-              src={lineItems[0].variant?.image.url}
-            />
-          </div>
-        ) : (
-          <div className="card-image aspect-square bg-primary/5">
-            <img
-              className="w-[168px] h-[168px] fadeIn cover"
-              src={placeholderImage || ''}
-              alt="placeholder"
-            />
-          </div>
-        )}
-        <div
-          className={`flex-col justify-center text-left ${
-            !lineItems[0].variant?.image && 'md:col-span-2'
-          }`}
-        >
-          <Heading as="h3" format className="items-center" size="copy">
-            {lineItems.length > 1
-              ? `${lineItems[0].title} +${lineItems.length - 1} more`
-              : lineItems[0].title}
-          </Heading>
-          <dl className="grid grid-cols-2 grid-gap-1">
-            {/* <dt className="text-[12px] md:text-[14px]">Order ID</dt> */}
-            <dd>
-              <Text className="text-[12px] md:text-[14px]" color="subtle">
-                Order No. {order.orderNumber}
-              </Text>
-            </dd>
-            {/* <dt className="text-[12px] md:text-[14px]">Order Date</dt> */}
-            <dd>
-              <Text className="text-[12px] md:text-[14px]">
-                {new Date(order.processedAt).toDateString()}
-              </Text>
-            </dd>
-            <dt className="sr-only">Fulfillment Status</dt>
-          </dl>
-        </div>
-      </Link>
-      <div className="self-end border-t">
+    <>
+      <li className="grid text-center shadow-lg">
         <Link
-          className=" flex justify-center block w-full p-2 text-center"
+          className=" flex md:grid items-center justify-center gap-4 p-4 md:gap-6 md:p-6  md:grid-cols-2"
           to={`/account/orders/${legacyOrderId}?${key}`}
           prefetch="intent"
         >
-          <DynamicButton
-            className=" bg-[#001a5f] px-[16px] w-full py-[12px] text-[16px]  text-[#fff]  hover:bg-[#EF6E6E]"
-            text="View Details"
-          />
+          {lineItems[0].variant?.image ? (
+            <div className="card-image aspect-square bg-primary/5">
+              <Image
+                width={168}
+                height={168}
+                className="w-full fadeIn cover"
+                alt={lineItems[0].variant?.image?.altText ?? 'Order image'}
+                src={lineItems[0].variant?.image.url}
+              />
+            </div>
+          ) : (
+            <div className="card-image aspect-square bg-primary/5">
+              <img
+                className="w-[168px] h-[168px] fadeIn cover"
+                src={placeholderImage || ''}
+                alt="placeholder"
+              />
+            </div>
+          )}
+          <div
+            className={`flex-col justify-center text-left ${
+              !lineItems[0].variant?.image && 'md:col-span-2'
+            }`}
+          >
+            <Heading as="h3" format className="items-center" size="copy">
+              {lineItems.length > 1
+                ? `${lineItems[0].title} +${lineItems.length - 1} more`
+                : lineItems[0].title}
+            </Heading>
+            <dl className="grid grid-cols-2 grid-gap-1">
+              {/* <dt className="text-[12px] md:text-[14px]">Order ID</dt> */}
+              <dd>
+                <Text className="text-[12px] md:text-[14px]" color="subtle">
+                  Order No. {order.orderNumber}
+                </Text>
+              </dd>
+              {/* <dt className="text-[12px] md:text-[14px]">Order Date</dt> */}
+              <dd>
+                <Text className="text-[12px] md:text-[14px]">
+                  {new Date(order.processedAt).toDateString()}
+                </Text>
+              </dd>
+              <dt className="sr-only">Fulfillment Status</dt>
+            </dl>
+          </div>
         </Link>
-      </div>
-    </li>
+        <div className="self-end border-t">
+          <Link
+            className=" flex justify-center block w-full p-2 text-center"
+            to={`/account/orders/${legacyOrderId}?${key}`}
+            prefetch="intent"
+          >
+            <DynamicButton
+              className=" bg-[#001a5f] px-[16px] w-full py-[12px] text-[16px]  text-[#fff]  hover:bg-[#EF6E6E]"
+              text="View Details"
+            />
+          </Link>
+        </div>
+      </li>
+    </>
   );
 }
 
