488dbc29d5e7b573a873f04b910af092dc67f5c4
sheetalsinghfabcode <sheetal@thefabcode.org>
2024-02-29 19:15:24 +0530

    working on fixing bugs

--- app/components/SucessfullLoader.jsx
+++ app/components/SucessfullLoader.jsx
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const SuccessfullLoader = ({successfullMessage}) => {
+const SuccessfullLoader = ({successfullMessage, circle}) => {
+  const [ellipsisVisible, setEllipsisVisible] = useState(true);
+  const [message, setMessage] = useState(successfullMessage);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setEllipsisVisible((prev) => !prev);
+    }, 1000); // Toggle visibility every second
+
+    return () => clearInterval(interval);
+  }, []);
+
+  // Effect to update the message when successfullMessage prop changes
+  useEffect(() => {
+    setMessage(successfullMessage);
+  }, [successfullMessage]);
+
   return (
-    <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center  items-center bg-black bg-opacity-80 z-50">
-      <div className="content   p-6 flex flex-col items-center justify-center rounded-lg shadow-xl">
-        <h4 className="text-[16px] md:text-[24px] lg:text-[36px] text-white font-bold text-center font-karla mb-6">
-          {successfullMessage}
-        </h4>
-        <svg className="mx-auto" width="300" height="300">
-          <circle
-            fill="none"
-            stroke="#68E534"
-            strokeWidth="20"
-            cx="150"
-            cy="150"
-            r="140"
-            strokeLinecap="round"
-            transform="rotate(-90 150 150)"
-            className="circle"
-          />
-          <polyline
-            fill="none"
-            stroke="#68E534"
-            points="65,164 130,224 228,98"
-            strokeWidth="18"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="tick"
-          />
-        </svg>
+    <div className="relative">
+      <div className="fixed top-0  left-0 right-0 bottom-0 flex justify-center items-center bg-black bg-opacity-80 z-50">
+        <div className="content p-6 flex min-h-screen flex-col items-center justify-center rounded-lg ">
+          <>
+            <h4 className="text-[24px] lg:text-[36px] text-white font-bold text-center fixed top-[15rem] md:top-[16rem] font-karla mb-6 transition-all duration-300 ease-in-out">
+              {message}
+              <span
+                className={`inline-block ${
+                  ellipsisVisible ? 'animate-bounce' : 'opacity-0'
+                }`}
+              >
+                ...
+              </span>
+            </h4>
+
+            {circle && (
+              <svg className="mx-auto " width="250" height="250">
+                <circle
+                  fill="none"
+                  stroke="#68E534"
+                  strokeWidth="20"
+                  cx="125"
+                  cy="125"
+                  r="115"
+                  strokeLinecap="round"
+                  transform="rotate(-90 125 125)"
+                  className="circle"
+                />
+                <polyline
+                  fill="none"
+                  stroke="#68E534"
+                  points="53,138 104,188 197,94"
+                  strokeWidth="18"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="tick"
+                />
+              </svg>
+            )}
+          </>
+        </div>
       </div>
     </div>
   );
