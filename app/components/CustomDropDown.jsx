8d0ba3583f6e80430da7a079cd43a9183a4a6b59
sheetalsinghfabcode <sheetal@thefabcode.org>
2024-04-29 15:59:23 +0530

    working on product-page-changes

--- app/components/CustomDropDown.jsx
+++ app/components/CustomDropDown.jsx
@@ -1,52 +1,183 @@
+import React, {useEffect, useRef, useState} from 'react';
 
-import React from 'react';
+// Icon component
+const Icon = ({isOpen}) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width="18"
+      height="18"
+      stroke="#222"
+      strokeWidth="1.5"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={isOpen ? 'translate' : ''}
+    >
+      <polyline points="6 9 12 15 18 9"></polyline>
+    </svg>
+  );
+};
 
-const CustomDropdown = ({ options, value, onChange }) => {
-  const renderOptions = (options) => {
-    return options.map((option, index) => {
-      if (Array.isArray(option)) {
-        // Render nested options
-        return (
-          <optgroup key={index} className='text-[16px]' label={option.label}>
-            {option?.map((nestedOption, nestedIndex) => (
-              <option
-                key={nestedIndex}
-                style={nestedOption?.style ? nestedOption?.style : {fontSize:"16px"}}
-                className={`${nestedOption.className}`}
-                value={nestedOption.value}
-              >
-                {nestedOption.label}
-              </option>
-            ))}
-          </optgroup>
-        );
-      } else {
-        // Render regular options
-        return (
-          <option
-            key={index}
-            style={option?.style}
-            className={`${option?.className}`}
-            value={option?.value}
-          >
-            {option?.label}
-          </option>
-        );
+// CloseIcon component
+const CloseIcon = () => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width="14"
+      height="14"
+      stroke="#fff"
+      strokeWidth="2"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="18" y1="6" x2="6" y2="18"></line>
+      <line x1="6" y1="6" x2="18" y2="18"></line>
+    </svg>
+  );
+};
+
+// CustomSelect component
+const CustomDropdown = ({
+  placeHolder,
+  options,
+  isMulti,
+  value,
+  isSearchable,
+  onChange,
+  align,
+}) => {
+  // State variables using React hooks
+  const [showMenu, setShowMenu] = useState(false); // Controls the visibility of the dropdown menu
+  const [selectedValue, setSelectedValue] = useState(isMulti ? [] : null); // Stores the selected value(s)
+  const searchRef = useRef(); // Reference to the search input element
+  const inputRef = useRef(); // Reference to the custom select input element
+
+  useEffect(() => {
+    if (showMenu && searchRef.current) {
+      searchRef.current.focus();
+    }
+  }, [showMenu]);
+
+
+  useEffect(() => {
+    const handler = (e) => {
+      if (inputRef.current && !inputRef.current.contains(e.target)) {
+        setShowMenu(false);
       }
-    });
+    };
+
+    window.addEventListener('click', handler);
+    return () => {
+      window.removeEventListener('click', handler);
+    };
+  });
+
+  const handleInputClick = (e) => {
+    setShowMenu(!showMenu);
   };
 
+  const getDisplay = () => {
+    if (!selectedValue || selectedValue.length === 0) {
+      return placeHolder;
+    }
+    if (isMulti) {
+      return (
+        <div className="dropdown-tags">
+          {selectedValue.map((option, index) => (
+            <div key={`${option?.value}-${index}`} className="dropdown-tag-item">
+              {option.label}
+              <span
+                onClick={(e) => onTagRemove(e, option)}
+                className="dropdown-tag-close"
+              >
+                <CloseIcon />
+              </span>
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return selectedValue.label;
+  };
+
+  const removeOption = (option) => {
+    return selectedValue.filter((o) => o.value !== option?.value);
+  };
+
+  const onTagRemove = (e, option) => {
+    e.stopPropagation();
+    const newValue = removeOption(option);
+    setSelectedValue(newValue);
+    onChange(newValue);
+  };
+
+  const onItemClick = (option) => {
+    let newValue;
+    if (isMulti) {
+      if (selectedValue.findIndex((o) => o.value === option?.value) >= 0) {
+        newValue = removeOption(option);
+      } else {
+        newValue = [...selectedValue, option];
+      }
+    } else {
+      newValue = option;
+    }
+    setSelectedValue(newValue);
+    onChange(newValue);
+  };
+
+  const isSelected = (option) => {
+    if (isMulti) {
+      return selectedValue.filter((o) => o.value === option?.value).length > 0;
+    }
+
+    if (!selectedValue) {
+      return false;
+    }
+
+    return selectedValue.value === option?.value;
+  };
+
+
+
+  console.log("options",options);
+
   return (
-    <div className="relative">
-      <select
-        className="appearance-none h-[40px] highlight-none cursor-pointer font-bold text-[14px] rounded border-0 border-black w-full font-inter text-sm text-[#737373]"
-        value={value}
-        onChange={onChange}
-      >
-        {renderOptions(options)}
-      </select>
+    <div className="custom--dropdown-container">
+      <div ref={inputRef} onClick={handleInputClick} className="dropdown-input">
+        <div
+          className={`dropdown-selected-value ${
+            !selectedValue || selectedValue.length === 0 ? 'placeholder' : ''
+          }`}
+        >
+          {getDisplay()}
+        </div>
+        <div className="dropdown-tools">
+          <div className="dropdown-tool">
+            <Icon isOpen={showMenu} />
+          </div>
+        </div>
+      </div>
+
+      {showMenu && (
+        <div className={`dropdown-menu alignment--${align || 'auto'}`}>
+         
+          {options.map((option) => (
+            <div
+            style={option?.style}
+              onClick={() => onItemClick(option)}
+              key={option?.value}
+              className={`dropdown-item ${option?.className} ${isSelected(option) && 'selected'}`}
+            >
+              {option?.label}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
 
-export default CustomDropdown;
+export default CustomDropdown
