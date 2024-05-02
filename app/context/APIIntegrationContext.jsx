1fda6e541cb48ec887fe695803351e41deab4e65
Ashish Sharma <ashish@thefabcode.org>
2024-05-02 19:23:05 +0530

    Added integrations page route

--- app/context/APIIntegrationContext.jsx
+++ app/context/APIIntegrationContext.jsx
@@ -3,7 +3,7 @@ import {createContext,  useContext, useState} from 'react';
 const APIIntegrationContext = createContext();
 
 export const APIIntegrationProvider = ({children}) => {
-  const [selectedSection, setSelectedSection] = useState('PRODUCTS');
+  const [selectedSection, setSelectedSection] = useState('API ENDPOINTS');
 
   const handleSectionChange = (event) => {
     const targetElementValue = event.target.value.toUpperCase();
