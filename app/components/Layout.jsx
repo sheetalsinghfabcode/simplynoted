4493a5d01e365b73da684cf3ea089690c8bf60b3
Ashish Sharma <ashish@thefabcode.org>
2024-05-15 12:53:45 +0530

    Aligned the dropdown with navbar links

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -962,13 +962,12 @@ const FlyoutLink = ({ children, href, FlyoutContent, data }) => {
             initial={{ opacity: 0, y: 15 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 15 }}
-            style={{ translateX: '-50%' }}
             transition={{ duration: 0.3, ease: 'easeOut' }}
-            className="absolute left-1/2 top-12 bg-white text-black"
+            className="absolute -left-[24px] top-[28px] bg-white text-black"
           >
-            <div className="absolute -top-6 left-0 right-0 h-6 bg-transparent" />
-            <div className="absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white" />
-            <FlyoutContent props={data} />
+            {/* <div className="absolute -top-6 left-0 right-0 h-6 bg-transparent" />
+            <div className="absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white" /> */}
+            <div className='w-[155px]'><FlyoutContent props={data} /></div>
           </motion.div>
         )}
       </AnimatePresence>
@@ -995,7 +994,7 @@ const PricingContent = ({ props }) => {
   }
 
   return (
-    <div className="w-[155px] bg-wheat p-6 shadow-xl">
+    <div className="bg-wheat p-6 shadow-xl">
       {props.map((item) => (
         <div key={item.id} className="mb-3 space-y-3 ">
           {item.to === "/customise-your-card" ?
