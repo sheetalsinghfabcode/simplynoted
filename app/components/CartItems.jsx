00be073ca37ab112508299dcfdc6d804ec17b0aa
Ashish Sharma <ashish@thefabcode.org>
2024-06-04 18:46:35 +0530

    Implemented package verification before proceeding with checkout

--- app/components/CartItems.jsx
+++ app/components/CartItems.jsx
@@ -1,7 +1,7 @@
 import {useEffect} from 'react';
 import {useStateContext} from '~/context/StateContext';
 
-const CartItems = ({id}) => {
+const CartItems = () => {
   const {
     setCartCountVal,
     cartCountVal,
@@ -11,31 +11,84 @@ const CartItems = ({id}) => {
   } = useStateContext();
 
   useEffect(() => {
-    // Read customerId from localStorage
-    const customerId = localStorage.getItem('customerId');
-
-    if (customerId) {
-      // Define the API URL with customerId
-      const apiUrl = `https://api.simplynoted.com/api/storefront/cart-items?customerId=${customerId}`;
-
-      fetch(apiUrl)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-
-        .then((data) => {
-          // Update state with fetched data
-          setCartCountVal(data.result.cartItems.length);
-          setCartData(data.result.cartItems);
-        })
-        .catch((error) => {
-          console.error('Error fetching data:', error);
-        });
-    }
+    handleCartUpdate();
   }, [isCartUpdated, cartCountVal, customerId]);
+
+  async function handleCartUpdate() {
+    const customerId = localStorage.getItem('customerId');
+    if (!customerId) return;
+
+    const hasPackageDiscountMatch = await crossVerifyPackageDiscount(
+      customerId,
+    );
+    if (!hasPackageDiscountMatch) {
+      return deleteCartItem(customerId);
+    }
+    await updateCartItems(customerId);
+  }
+
+  async function updateCartItems(customerId) {
+    try {
+      const URL = `https://api.simplynoted.com/api/storefront/cart-items?customerId=${customerId}`;
+      const response = await fetch(URL);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      if (data.result.success) {
+        setCartCountVal(data.result.cartItems.length);
+        setCartData(data.result.cartItems);
+      }
+    } catch (err) {
+      console.error(`Error fetching data: ${err}`);
+    }
+  }
+
+  async function deleteCartItem(customerId) {
+    try {
+      const URL = `https://api.simplynoted.com/api/storefront/cart-items/delete?customerId=${customerId}`;
+      const response = await fetch(URL, {method: 'POST'});
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      if (data.result.success) {
+        setCartCountVal(0);
+      }
+    } catch (err) {
+      console.error(`Error deleting cart item: ${err}`);
+    }
+  }
+
+  async function crossVerifyPackageDiscount(customerId) {
+    try {
+      const localPackageDiscount = JSON.parse(
+        localStorage.getItem('packageDiscount'),
+      );
+      if (!localPackageDiscount) return true;
+      const URL = `https://api.simplynoted.com/stripe/customer-data?customerId=${customerId}`;
+      const response = await fetch(URL);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      if (data?.stripe) {
+        const currentPackageDiscount = data?.stripe.packageDiscount;
+        if (currentPackageDiscount != localPackageDiscount) {
+          localStorage.setItem(
+            'packageDiscount',
+            JSON.stringify(data.stripe.packageDiscount),
+          );
+          return false;
+        }
+      }
+      return true;
+    } catch (err) {
+      console.error(`Error deleting cart item: ${err}`);
+    }
+  }
 
   return <></>;
 };
