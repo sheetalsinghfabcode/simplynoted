26908a6c9135e4aedf7fd1e5d3694794cfce5c92
Ashish Sharma <ashish@thefabcode.org>
2024-06-13 15:10:41 +0530

    Integrated clarity to the storefront

--- app/root.jsx
+++ app/root.jsx
@@ -121,7 +121,6 @@ export default function App() {
             name="google-site-verification"
             content="x04VnoyVjG2G7tiib1qgc7EnE7fyoRzdXNwhgg3G_Wk"
           />
-          
           <meta name="viewport" content="width=device-width,initial-scale=1" />
 
           <meta
@@ -159,6 +158,21 @@ export default function App() {
             async=""
             src="https://www.googletagmanager.com/gtm.js?id=GTM-PB7P5VV"
           ></script>
+
+          <script type="text/javascript">
+            {(function (c, l, a, r, i, t, y) {
+              c[a] =
+                c[a] ||
+                function () {
+                  (c[a].q = c[a].q || []).push(arguments);
+                };
+              t = l.createElement(r);
+              t.async = 1;
+              t.src = 'https://www.clarity.ms/tag/' + i;
+              y = l.getElementsByTagName(r)[0];
+              y.parentNode.insertBefore(t, y);
+            })(window, document, 'clarity', 'script', 'mr8fm8dv9t')}
+          </script>
 
           <Seo />
           <Meta />
